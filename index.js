96095543c8df19a01e0a1978922c736fd2696f86
Jasonnonstop10 <jay44411@gmail.com>
2018-04-12 23:18:25 +0700

    web  80%
    speed web

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
-var list=[];
+let list=[];
 
 function Prayut(){
-    alert("You choose M44 of Prayut Chan-o-cha");
+    alert("You choose M44 of Prayut Chan-o-cha")
     list[0]="You choose M44 of Prayut Chan-o-cha";
 }
-
-function Prawit()(){
-    alert("You choose Ring and Watch of Prawit Wongsuwan");
+function Prawit(){
+    alert("You choose Ring and Watch of Prawit Wongsuwan")
     list[0]="You choose Ring and Watch of Prawit Wongsuwan";
 }
 function Thaksin(){
-    alert("You choose Thaicom of Thaksin Shinawatra");
+    alert("You choose Thaicom of Thaksin Shinawatra")
     list[0]="You choose Thaicom of Thaksin Shinawatra";
 }
+function Continue() {
+    if (document.getElementById('g1').checked) {
+    list[2] = document.getElementById('g1').value;
+  } else if (document.getElementById('g2').checked) {
+    list[2] = document.getElementById('g2').value;
+  } else if (document.getElementById('g3').checked) {
+    list[2] = document.getElementById('g3').value;
+  }
+    list[3] = document.getElementById('namecard').value;
+    list[4] = document.getElementById('numbercard').value;
+  
+    if (list[0] === undefined) {
+      alert("Please check Poster choice")
+      wait(2000);
+    } else if (list[2] === undefined) {
+      alert("Please check your card type");
+      wait(2000);
+    } else if (list[3] === "") {
+      alert("Please check name of card");
+      wait(2000);
+    } else if (list[4] === ""){
+      alert("Please check number of card");
+      wait(2000);
+    }
+  
+    if (confirm("Are you want to buy?")===true) {
+      document.getElementById("c").innerHTML = list[0]+"<br>"+" by "
+            +list[2]+" Name of Card is : "+list[3]
+            +" ,Number of Card is : "+list[4];
+    }
+  
+  }
