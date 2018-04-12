41f97650fd35934bb26fed55f4a92ad6f601ba44
Jasonnonstop10 <jay44411@gmail.com>
2018-04-12 22:09:33 +0700

    web 70%
    ready on yet

---
+++ index.js
@@ -0,0 +1,15 @@
+var list=[];
+
+function Prayut(){
+    alert("You choose M44 of Prayut Chan-o-cha");
+    list[0]="You choose M44 of Prayut Chan-o-cha";
+}
+
+function Prawit()(){
+    alert("You choose Ring and Watch of Prawit Wongsuwan");
+    list[0]="You choose Ring and Watch of Prawit Wongsuwan";
+}
+function Thaksin(){
+    alert("You choose Thaicom of Thaksin Shinawatra");
+    list[0]="You choose Thaicom of Thaksin Shinawatra";
+}
