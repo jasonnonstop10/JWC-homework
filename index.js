b9306a1853d3ec7abc9b03528a4dace5fb036ed4
Jasonnonstop10 <jay44411@gmail.com>
2018-04-13 12:15:36 +0700

    all save
    ???

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
-let list=[];
+let li=[];
 
 function Prayut(){
     alert("You choose M44 of Prayut Chan-o-cha")
-    list[0]="You choose M44 of Prayut Chan-o-cha";
+    li[0]="M44 of Prayut Chan-o-cha";
 }
 function Prawit(){
     alert("You choose Ring and Watch of Prawit Wongsuwan")
-    list[0]="You choose Ring and Watch of Prawit Wongsuwan";
+    li[0]="Ring and Watch of Prawit Wongsuwan";
 }
 function Thaksin(){
     alert("You choose Thaicom of Thaksin Shinawatra")
-    list[0]="You choose Thaicom of Thaksin Shinawatra";
+    li[0]="Thaicom of Thaksin Shinawatra";
 }
 function Gg() {
     if (document.getElementById('g1').checked) {
-    list[2] = document.getElementById('g1').value;
+    li[2] = document.getElementById('g1').value;
   } else if (document.getElementById('g2').checked) {
-    list[2] = document.getElementById('g2').value;
+    li[2] = document.getElementById('g2').value;
   } else if (document.getElementById('g3').checked) {
-    list[2] = document.getElementById('g3').value;
+    li[2] = document.getElementById('g3').value;
   }
-    list[3] = document.getElementById('namecard').value;
-    list[4] = document.getElementById('numbercard').value;
+    li[3] = document.getElementById('namecard').value;
+    li[4] = document.getElementById('numbercard').value;
   
-    if (list[0] === undefined) {
+    if (li[0] === undefined) {
       alert("Please check Item choice")
       wait(2000);
-    } else if (list[2] === undefined) {
+    } else if (li[2] === undefined) {
       alert("Please check your Cryptocurrency type");
       wait(2000);
-    } else if (list[3] === "") {
+    } else if (li[3] === "") {
       alert("Please check Name of account");
       wait(2000);
-    } else if (list[4] === ""){
+    } else if (li[4] === ""){
       alert("Please check Pubile key number");
       wait(2000);
     }
   
     if (confirm("Are you want to buy?")===true) {
-      document.getElementById("c").innerHTML ="Buy "+list[0]+"<br>"+" by "
-            +list[2]+" Name of account is : "+list[3]
-            +" ,Pubile key number is: "+list[4];
+      document.getElementById("j").innerHTML ="Buy "+li[0]+"<br>"+" by "
+            +li[2]+" Name of account is : "+li[3]
+            +" ,Pubile key number is: "+li[4];
     }
   
   }
